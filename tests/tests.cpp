44dccb92d3a900f0064dab47489ad7295a6e80a6
Sergei Alexeev <serenehet@gmail.com>
2020-03-20 00:49:28 +0300

    save changes

---
+++ tests/tests.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+extern "C" {
+//#include "dz.h"
+}
+
+TEST(testAreaComparison, test1) {
+    ASSERT_EQ(true, true);
+}
--- unitTets/main.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by Sergei Alexeev on 20.03.2020.
-//"hhbbcccdddyyytttooo"
-
